f63eeb9ec11edee1acbeb4cbac93f5c9b2e2204c
Alexandr <55022191+HartoSha@users.noreply.github.com>
2020-11-20 20:00:55 +0400

    Cleanup usings

--- 5HeadBot/Modules/Public/HelpModule.cs
+++ 5HeadBot/Modules/Public/HelpModule.cs
@@ -3,7 +3,6 @@ using _5HeadBot.Services.Core.BotMessageService;
 using _5HeadBot.Services.Core.BotMessageService.Data;
 using Discord;
 using Discord.Commands;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
